879c46cbae3850fdb170c0ec37a681176cdb38a3
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-06 01:58:25 +0700

    add navbar component dari boostatp dan nambahin link data

--- src/components/NavbarHerMoney.jsx
+++ src/components/NavbarHerMoney.jsx
@@ -1,6 +1,28 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { navLinks } from "../data/index";
+import { NavLink } from "react-router-dom";
+
 const NavbarHerMoney = () => {
   return (
-    <div>NavbarHerMoney</div>
+    <div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            {navLinks.map((link) => {
+              return (
+                <div key={link.id}>
+                   <NavLink to={link.path}>{link.text}</NavLink>
+                </div>
+              );
+            })}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
   )
 }
 
