4fceda1d4940ee9ae5e257132807c1ee442fa4a7
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-06 00:31:39 +0700

    add component navbar dan footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import NavbarHerMoney from "./components/NavbarHerMoney";
 import HomepageHerMoney from "./pages/HomepageHerMoney";
 import KelasHerMoney from "./pages/KelasHerMoney";
 import TestimonialHerMoney from "./pages/TestimonialHerMoney";
 import SyaketenPage from "./pages/SyaketenPage";
 import FaqPageHerMoney from "./pages/FaqPageHerMoney";
+import FooterHerMoney from "./components/FooterHerMoney";
 
 function App() {
   return (
     <div>
+      <NavbarHerMoney />
+
       <Routes>
         <Route path="/" Component={HomepageHerMoney} />
         <Route path="/kelas" Component={KelasHerMoney} />
@@ -15,6 +19,8 @@ function App() {
         <Route path="/faq" Component={FaqPageHerMoney} />
         <Route path="/syaratketentuan" Component={SyaketenPage} />
       </Routes>
+
+      <FooterHerMoney />
     </div>
   );
 };
