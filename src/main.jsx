46c4b565f249d9cd6fbfc4a7b6f97ab81fae7b84
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-06 01:56:13 +0700

    add file boostrap react

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
