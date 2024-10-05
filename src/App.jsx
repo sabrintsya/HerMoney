e757bc4e218155cda8ccaf1a01d6816be1739005
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-06 00:12:22 +0700

    import file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <div>hello world</div>
-}
+import { Routes, Route } from "react-router-dom";
+import HomepageHerMoney from "./pages/HomepageHerMoney";
+import KelasHerMoney from "./pages/KelasHerMoney";
+import TestimonialHerMoney from "./pages/TestimonialHerMoney";
+import FaqHerMoney from "./components/FaqHerMoney";
+import SyaketenPage from "./pages/SyaketenPage";
 
-export default App
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" Component={HomepageHerMoney} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
