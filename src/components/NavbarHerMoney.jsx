d3adcbc7050f10d22ebe674899f0df28bf72272a
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-06 07:36:59 +0700

    menambahkan usestate dan useeffect styling button dan navlink

--- src/components/NavbarHerMoney.jsx
+++ src/components/NavbarHerMoney.jsx
@@ -1,24 +1,49 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav} from "react-bootstrap";
 import { navLinks } from "../data/index";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-const NavbarHerMoney = () => {
+const NavbarHerMoney = () => { 
+  const [changeColor, setChangeColor] = useState(false);
+
+  const changeBackgroundColor = () => {
+if (window.scrollY > 10 ) {
+  setChangeColor(true);
+} else {
+  setChangeColor(false);
+}
+  };
+
+  useEffect(() => {
+    changeBackgroundColor();
+
+    window.addEventListener('scroll', changeBackgroundColor);
+  });
+
   return (
     <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home" className="fs-3 fw-bold">
+        HerMoney.
+      </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
+          <Nav className="mx-auto text-center">
             {navLinks.map((link) => {
               return (
-                <div key={link.id}>
-                   <NavLink to={link.path}>{link.text}</NavLink>
+                <div className="nav-link" key={link.id}>
+                   <NavLink to={link.path} className={({ isActive, isPending }) => 
+                   isPending ? "pending" : isActive ? "active" : ""
+                   } end >{link.text}</NavLink>
                 </div>
               );
             })}
           </Nav>
+
+          <div className="text-center">
+            <button className="btn btn-outline-success rounded-1">Gabung</button>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
