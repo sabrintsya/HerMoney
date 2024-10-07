b216633bccb38416add540c2e681a1ce44db7174
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-07 23:04:17 +0700

    add component faq hermoney

--- src/components/FaqHerMoney.jsx
+++ src/components/FaqHerMoney.jsx
@@ -1,7 +1,35 @@
+import { Container, Row, Col, Accordion} from 'react-bootstrap';
+import { faq } from "../data/index.js"
+
+
 const FaqHerMoney = () => {
   return (
-    <div>FaqHerMoney</div>
-  )
+    <div className="faq">
+      <Container>
+        <Row>
+          <Col>
+          <h2 className="text-center">Pertanyaan Yang sering ditanyakan</h2>
+          </Col>
+        </Row>
+        <Row className="row-col-lg-2 row-col-1 g-4 pt-5 "> 
+          {faq.map((data) => {
+          return (
+            <Col key={data.id}>
+            <Accordion className="shadow-sm">
+            <Accordion.Item eventKey={data.eventKey}>
+        <Accordion.Header>{data.title}</Accordion.Header>
+        <Accordion.Body>
+          {data.desc}
+        </Accordion.Body>
+      </Accordion.Item>
+      </Accordion>
+            </Col>
+          );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default FaqHerMoney;
