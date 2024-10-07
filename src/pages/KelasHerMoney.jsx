28f76bdf2ed147ba0c953d13a738bfcd0147c180
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-07 23:08:16 +0700

    add component for kelas

--- src/pages/KelasHerMoney.jsx
+++ src/pages/KelasHerMoney.jsx
@@ -1,6 +1,18 @@
+import {Container, Row, Col} from "react-bootstrap";
+import { semuaKelas } from "../data";
+
 const KelasHerMoney = () => {
   return (
-    <div>KelasHerMoney</div>
+    <div className="kelas min-vh-100">
+      <Container>
+        <Row>
+          <Col>
+          <h1 className="fw-bold text-center">Semua Kelas</h1>
+          <p className="text-center">Kuasi Keuanganmu, Raih Kebebasan Finansial!</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
