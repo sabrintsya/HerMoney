7da9cce5c8050b7e92005c61f1105cee90bcae12
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-08 01:33:35 +0700

    add scroll on top and animate css

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App  from "./App.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.css';
+import 'animate.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
     <App />
     </BrowserRouter>
   </StrictMode>,
